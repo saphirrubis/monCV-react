9d341738bc5c76e3615382e0c4e92afe9623a755
Saphir <t.dubus59@gmail.com>
2023-04-21 15:48:27 +0200

    maj exp + cv

--- src/components/knowledges/Expriences.js
+++ src/components/knowledges/Expriences.js
@@ -4,32 +4,42 @@ const Expriences = () => {
   return (
     <div className="experience">
     <div className="exp-0">
-      <h4>L'incubateur - 1 job-1 formation (Euratechnologie)</h4>
-      <h5>15/03/2023 </h5>
+      <h4>L'incubateur Numérique (Euratechnologie)</h4>
+      <h5>15/03/2023 à ce jour  </h5>
       <p>
-      Mise en situtation professionnelle de développeuse web, web mobile.
-      Création d’un site e-commerce Fullstack avec les langages Symfony/React.
+      Contexte: Projet de site de Covoiturage <br/>
+Taches: Travailler en méthode agile<br/>
+        Développer une API back-end<br/>
+Environnement technique: Figma, Jira, Miro, Looping, Mysql, GIT, Symfony,React<br/>
       </p>
     </div> 
   
     <h3> Expériences</h3>
-    <div className="exp-1">
+    <div className="exp-2">
       <h4>Développeuse Web - Benoît Ly pédicure-podologue</h4>
       <h5>17/10/2022 au 20/01/2023</h5>
       <p>
-      Création de la  maquette du site sur figma,
-      Réalisation du site vitrine en Sinlge Page Application avec le langage REACT-JS.
+      Contexte: projet de site vitrine <br/>
+Taches:Réalisation de la maquette<br/>
+       Création du sites<br/>
+Environnement technique:Figma, Visual code,React-JS<br/>
       </p>
     </div> 
-   <div className="exp-2">
-      <h4>Projet personnel (en cours)</h4>
-      <h5>05/02/2023</h5>
+   <div className="exp-1">
+      <h4>Projet personnel</h4>
+      <h5>05/02/2023  au 28/02/2023</h5>
       <p>
-      J'ai amélioré le front-end du site e-commerce fait sur WEBADOR d'une amie, pour le rendre plus dynamique et attractif pour l’utilisateur.
+      Contexte: Amélioration d'un site e-commerce<br/>
+Taches: amélioration du front<br/>
+Environnement technique: sur le site WEBADOR<br/>
     </p>
-   <h5>2023</h5>
+   <h5>Depuis 2023 </h5>
       <p>
-      Mon projet est un site vitrine/blog avec les langages React pour le front /Symfony.
+      Contexte:Création d'un site vitrine-blog<br/>
+Taches:Réalisation de la maquette<br/>
+Réalisation du front du site<br/>
+Réalisation de MCD<br/>
+Environnement technique:Mysql, Symfony, React<br/>
       </p>
     </div> 
     <div className="exp-3">
--- src/components/knowledges/Languages.js
+++ src/components/knowledges/Languages.js
@@ -4,14 +4,14 @@ import ProgressBar from './ProgressBar';
 class Languages extends Component {
   state={
     languages: [
-      { id:1, value:"Javascript", xp:1},
-      { id:2, value:"Html/Css", xp:1.05},
-      { id:3, value:"PHP", xp:.94}
+      { id:1, value:"Javascript", xp:1.15},
+      { id:2, value:"Html/Css", xp:1.30},
+      { id:3, value:"PHP", xp:1}
     ],
     frameworks:[
-      { id:1, value:"React", xp:.85},
+      { id:1, value:"React", xp:1},
       { id:2, value:"Angular", xp:.10},
-      { id:3, value:"Symfony", xp:.75}
+      { id:3, value:"Symfony", xp:.95}
     ]
   }
   render() {
